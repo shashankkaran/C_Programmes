640ccce8241362f021a416197fbb83a9e321b72b
Shashank Karan <shashankkaran999@gmail.com>
2020-10-12 09:01:56 +0530

    divisibility test

---
+++ divisibily test.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+
+ main(){
+
+
+  int a , b ;
+ printf("write the number which is divided by another number\n");
+ scanf( "%d" , &a);
+
+ printf("write the number which divides previous number\n") ;
+ scanf( "%d",&b);
+
+
+
+
+if(a%b==0){
+printf(" %d is divisible by %d " , a ,b ) ;
+
+
+
+}
+else{
+printf(" %d is   not  divisible by %d " , a ,b ) ;
+
+
+
+}
+
+
+
+
+
+ return 0;
+ }
