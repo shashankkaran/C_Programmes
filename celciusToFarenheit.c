238d8e11a08f24bba0ab0e0e31ec7422a6242636
Shashank Karan <shashankkaran999@gmail.com>
2020-10-12 08:57:53 +0530

    first commit

---
+++ celciusToFarenheit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ main(){
+ float faren , cel ;
+ printf("write temperature in farenheight to convert it into celcius \n");
+ scanf("%f",&faren) ;
+ printf("the temp in celcius is %f ", (faren-32)*5/9);
+
+
+
+ return 0;
+ }
